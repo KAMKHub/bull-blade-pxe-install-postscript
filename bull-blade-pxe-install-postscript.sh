a82004098c89bb36a8e519c4fdadc6983797c8e2
Jukka Jurvansuu <jukka.jurvansuu@kamk.fi>
2019-01-27 23:59:37 +0200

    bull-blade-pxe-install-postscript.sh updated.

--- bull-blade-pxe-install-postscript.sh
+++ bull-blade-pxe-install-postscript.sh
@@ -11,15 +11,28 @@
 #OS:           CentOS 7
 #==============================================================================
 
-### Install tools and useful utilities
+#==============================================================================
+# Install tools and useful utilities
+#==============================================================================
+
 yum install -y epel-release
+yum install -y telnet
 yum update -y
 
-### Install NVIDIA Driver
+# Disable SELinux
+sed -i 's/enforcing/disabled/g' /etc/selinux/config /etc/selinux/config
+
+# Disable Firewall
+systemctl stop firewalld
+systemctl disable firewalld
+
+#==============================================================================
+# Install NVIDIA Driver
+#==============================================================================
 
 # Development tools should be installed at the Kickstart for the NVIDIA script because this install cannot find
 ## kernel-devel and kernel-headers for the default kernel version
-# yum groupinstall -y "Development Tools"
+# yum groupinstall -y "Development tools"
 # yum install -y kernel-devel-$(uname -r)
 # yum install -y kernel-headers-$(uname -r)
 
@@ -33,17 +46,28 @@ sed -ri 's/GRUB_CMDLINE_LINUX="/GRUB_CMDLINE_LINUX="nouveau.modeset=0 rd.driver.
 grub2-mkconfig -o /boot/grub2/grub.cfg
 grub2-mkconfig -o /boot/efi/EFI/centos/grub.cfg
 
-# Install NVIDIA driver
+# A) Install NVIDIA driver from the source
 cd /root
 yum install -y dkms
 wget http://fr.download.nvidia.com/tesla/410.79/NVIDIA-Linux-x86_64-410.79.run
 sh NVIDIA-Linux-x86_64-410.79.run -s
 
+# B) Install NVIDIA driver and CUDA Toolkit using repository
+## NOT WORKING WITH CENTOS 7
+# cd /root
+# wget https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-repo-rhel7-10.0.130-1.x86_64.rpm
+# rpm -i cuda-repo-*.rpm
+# yum install cuda
+# export PATH=/usr/local/cuda/bin:$PATH
+# export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+
 # Info: How to test NVIDIA driver
 # lshw -numeric -C display
 # nvidia-smi
 
-### Docker installation
+#==============================================================================
+# Docker installation
+#==============================================================================
 
 # Install required packages for Docker
 yum install -y yum-utils \
